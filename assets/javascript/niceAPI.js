a5d0750a0a8555f2eb6a5654eda19a8b97d5d3ea
Jenna Miles <jennamiles@Jennas-MacBook-Pro.local>
2019-12-14 13:17:16 -0700

    added quote api and got it working on the sweet html page

--- assets/javascript/niceAPI.js
+++ assets/javascript/niceAPI.js
@@ -1,8 +1,15 @@
-let queryUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+let queryUrl = 'http://quotes.rest/qod.json?category=inspire';
 
 $.ajax ({
     url: queryUrl,
     method: 'GET'
 }).then(function(response){
     console.log(response);
+    let quote = response.contents.quotes[0].quote;
+    let author = response.contents.quotes[0].author;
+
+    $('#quoteArea').html(quote);
+    $('#quoteAuthor').html(author);
 });
+
